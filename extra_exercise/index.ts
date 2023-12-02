d52dda84d5e5a0a39d6e8379b9a3bfad17bb6d39
ValeryPotrebina <valery_potrebina_2003@mail.ru>
2023-12-02 17:48:46 +0300

    improved

--- extra_exercise/index.ts
+++ extra_exercise/index.ts
@@ -1,19 +1,26 @@
 //  Компилятор не указывает ни одной строки, где реально есть ошибки
 // но указывает, как на ошибочные хотя бы на 2 строки, где на самом деле ошибок нет (6 баллов)
-  interface A{
-    a: string
+
+
+
+interface A{
+  a: string[]
   }
-  interface B extends A {
+
+  interface B {
+    a: A[]
+  }
+
+  interface C extends B{
     b: string
   }
-  const b: B = {
-    a: 'a',
-    b: 'b'
-  }
   
-  const f1 = (): number => 2
-  const f2 = f1  //ERROR (need - ";")
-  (b as A).a // compiler indicated an error here, although it is not here
+  function test(c: C): number{
+    const f1 = (b: B) => b.a[0]
+    const f2 = f1                    //missed ;
+    (c as B).a.map(e => e.a)
 
+    return f2(c as B).a.length
 
-
+  }
+ 
